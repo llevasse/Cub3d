4f0dc5a3530df27fa7efd433281063d9ba226ca9
llevasse <leonardlevasseur@yahoo.com>
2023-09-23 22:07:55 +0200

    idk but it's cool

--- srcs/render/collision.c
+++ srcs/render/collision.c
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 01:32:27 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/23 22:02:26 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/23 22:06:20 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	check_w_smaller_mov(t_cub *cub, int angle, float *offset)
 		*offset -= 1;
 	else
 		*offset += 1;
+	if (*offset == 0)
+		return (1);
 	return (check_collision(cub, angle, offset));
 }
 
@@ -28,9 +30,7 @@ int	check_collision(t_cub *cub, int angle, float *offset)
 	float	px;
 	float	py;
 
-	get_player_new_pos(cub, angle, *offset, &new_p);		//get new possition without setting them to the player
-	if (*offset == 0)
-		return (0);
+	get_player_new_pos(cub, angle, *offset, &new_p);
 	if (new_p.x < 0 || new_p.y < 0 || new_p.x > MINIMAP_WIDTH || new_p.y > MINIMAP_HEIGHT)
 		return (1);
 	px = cub->player.px;
