abe369e3575718e07a62c6b08152f3760d71dfc4
llevasse <leonardlevasseur@yahoo.com>
2023-09-23 17:09:22 +0200

    norm

--- srcs/render/draw_fov.c
+++ srcs/render/draw_fov.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 21:44:52 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/23 17:04:39 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/23 17:06:23 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ void	draw_fov(t_cub *cub)
 	t_point	p;
 	t_point	p2;
 	t_line	false_line;
-	int		colour;
 
 	fov_angle = no_higher(-(PLAYER_FOV / 2), 360, 0);
 	fov_end_a = no_higher(fov_angle + PLAYER_FOV, 360, 0);
-	colour = PLAYER_RGB;
 	while (fov_angle != fov_end_a)
 	{
 		get_player_new_pos(cub, fov_angle, MINIMAP_HEIGHT * 10, &p);
@@ -32,11 +30,10 @@ void	draw_fov(t_cub *cub)
 		false_line = get_line(p, p2);
 		while (false_line.steps-- > 0)
 		{
-			draw_line(*cub, (int)p.x, (int)p.y, colour);
+			draw_line(*cub, (int)p.x, (int)p.y, 0x222222);
 			p.x += false_line.x_step;
 			p.y += false_line.y_step;
 		}
-		colour = 0x222222;
 	}
 }
 // fov_end_a for fov_end_angle!
