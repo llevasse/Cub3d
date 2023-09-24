18ddf924e0a0d84b051b2918946d6eb61aa3bec6
llevasse <leonardlevasseur@yahoo.com>
2023-09-24 23:09:39 +0200

    yeah wallsss :D

--- srcs/render/casting.c
+++ srcs/render/casting.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 22:25:17 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/24 22:51:20 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/24 23:08:21 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ void	cast(t_cub *cub, int dist, int x)
 	int	low_y;
 	int	height;
 
-	height = (WINDOW_H - dist) / 2;
+	height = (cub->mmap->block_h * WINDOW_H) / dist;
 	high_y = (WINDOW_H / 2) - height;
 	low_y = (WINDOW_H / 2) + height;
+	if (high_y < 0)
+		high_y = 0;
+	if (low_y > WINDOW_H)
+		low_y = WINDOW_H;
 	while (high_y < low_y)
 	{
 		if (get_pixel_colour(&cub->img, x, high_y) != MMAP_W_RGB && \
