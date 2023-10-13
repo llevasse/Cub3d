c9c65b46e3ed76980349b7387bba26d22675f097
llevasse <leonardlevasseur@yahoo.com>
2023-10-13 10:15:43 +0200

    seems a bit smoother

--- headers/cub3d.h
+++ headers/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 21:43:29 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/28 22:56:22 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/13 10:14:19 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,11 @@
 
 # include <stdio.h> //debug.
 
-# define WINDOW_W	1024*2
-# define WINDOW_H	512*2
+//# define WINDOW_W	1024*2
+//# define WINDOW_H	512*2
+
+# define WINDOW_W	320*3
+# define WINDOW_H	200*3
 
 # define MMAP_W	WINDOW_W/3
 # define MMAP_H	WINDOW_H/3
