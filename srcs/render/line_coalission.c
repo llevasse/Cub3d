1a491ca297a33491de98d6dc21b846213c40a5d5
llevasse <leonardlevasseur@yahoo.com>
2023-10-26 18:54:34 +0200

    get right begin pos for get_horr()

--- srcs/render/line_coalission.c
+++ srcs/render/line_coalission.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 21:58:31 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/26 11:15:30 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/26 18:54:04 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ int		init_horr(t_cub cub, float pa, t_line *line){
 
 	printf("pa %f\n", pa);
 	Tan = 1.0/tan(pa * RADIAN);
-	if (pa > 180 && pa < 360)
+	if (pa > 180 && pa < 360) //if ray is facing up
 	{
 		line->y_step = cub.mmap->block_s;
-		line->p_a.y =(cub.player.py/cub.mmap->block_s) * cub.mmap->block_s + cub.mmap->block_s;
-		line->p_a.x = ((cub.player.py - line->p_a.y) * Tan) + cub.player.px;
+		line->p_a.y =((int)cub.player.py/cub.mmap->block_s) * cub.mmap->block_s - 1;
+		line->p_a.x = ((cub.player.py - line->p_a.y) * Tan + cub.player.px);
 		line->x_step = -line->y_step * Tan;
 		return (8);
 	}
-	else if (pa > 0 && pa < 180)
+	else if (pa > 0 && pa < 180) // if ray is facing down
 	{
 		line->y_step = -cub.mmap->block_s;
-		line->p_a.y =(cub.player.py/cub.mmap->block_s) * cub.mmap->block_s - 0.0001;
-		line->p_a.x = ((cub.player.py - line->p_a.y) * Tan) + cub.player.px;
+		line->p_a.y =((int)cub.player.py/cub.mmap->block_s) * cub.mmap->block_s + cub.mmap->block_s;
+		line->p_a.x = ((cub.player.py - line->p_a.y) * Tan + cub.player.px);
 		line->x_step = -line->y_step * Tan;
 		return (8);
 	}
-	else
+	else // if ray is facing straight left or right (pa == 180 || pa == 360 || pa == 0)
 	{
 		line->p_a.x = cub.player.px;
 		line->p_a.y = cub.player.py;
@@ -68,7 +68,7 @@ t_line	get_horr(t_cub cub, float pa, float ca)
 	}
 	line = get_line(get_player_point(cub.player.px, cub.player.py), line.p_a);
 	ret = line;
-	while (line.steps > 0 && line.p_a.x >= 0 && line.p_a.x <= WINDOW_W && \
+/*	while (line.steps > 0 && line.p_a.x >= 0 && line.p_a.x <= WINDOW_W && \
 			line.p_a.y >= 0 && line.p_a.y <= WINDOW_H)
 	{
 		pos_x = (line.p_a.x / cub.mmap->block_s);
@@ -79,7 +79,7 @@ t_line	get_horr(t_cub cub, float pa, float ca)
 		line.p_a.x += line.x_step;
 		line.p_a.y += line.y_step;
 		line.steps--;
-	}
+	}*/
 	if (dof == -42)
 		ret.dist = (float)0x7fffffff;
 	return(ret);
