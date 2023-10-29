1fd76322c4147b3405410052a97aec4d2e118bde
llevasse <leonardlevasseur@yahoo.com>
2023-10-29 18:17:39 +0100

    rm read of uninitalized value

--- srcs/render/line_coalission.c
+++ srcs/render/line_coalission.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 21:58:31 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/29 15:33:03 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/29 18:17:13 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,8 @@ t_line	get_horr(t_cub cub, float pa)
 	{
 		pos_x = (line.p_a.x / cub.mmap->block_s);
 		pos_y = (line.p_a.y / cub.mmap->block_s);
-		if (pos_y >= cub.mmap->nb_line \
+		printf("%d %d (%d)\n", pos_x, pos_y, cub.mmap->nb_line);
+		if (pos_y >= cub.mmap->nb_line || pos_y < 0 || pos_x < 0 || pos_x >= (int)ft_strlen(cub.mmap->map[pos_y]) \
 			|| !ft_is_in_str("NSEW0", cub.mmap->map[pos_y][pos_x]))
 			break ;
 		line.p_a.x += line.x_step;
@@ -112,7 +113,7 @@ t_line	get_vert(t_cub cub, float pa)
 	{
 		pos_x = (line.p_a.x / cub.mmap->block_s);
 		pos_y = (line.p_a.y / cub.mmap->block_s);
-		if (pos_y >= cub.mmap->nb_line || pos_y < 0 || pos_x < 0 \
+		if (pos_y >= cub.mmap->nb_line || pos_y < 0 || pos_x < 0 || pos_x >= (int)ft_strlen(cub.mmap->map[pos_y]) \
 			|| !ft_is_in_str("NSEW0", cub.mmap->map[pos_y][pos_x]))
 			break ;
 		line.p_a.x += line.x_step;
