57690fc2c116a11cdd4826c3dcfd26092288733a
llevasse <leonardlevasseur@yahoo.com>
2023-12-16 19:21:15 +0100

    normed srcs/render/get_orientation.c srcs/render/render.c

--- srcs/render/get_orientation.c
+++ srcs/render/get_orientation.c
@@ -6,15 +6,16 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:34:04 by llevasse          #+#    #+#             */
-/*   Updated: 2023/11/29 19:07:22 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/12/16 19:20:18 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_img *get_orient_horr(t_map *map, float ca, int *side)
+t_img	*get_orient_horr(t_map *map, float ca, int *side)
 {
-	if (ca > 180 && ca < 360){
+	if (ca > 180 && ca < 360)
+	{
 		*side = NO;
 		return (&map->north_img);
 	}
@@ -22,9 +23,10 @@ t_img *get_orient_horr(t_map *map, float ca, int *side)
 	return (&map->south_img);
 }
 
-t_img *get_orient_vert(t_map *map, float ca, int *side)
+t_img	*get_orient_vert(t_map *map, float ca, int *side)
 {
-	if (ca > 90 && ca < 270){
+	if (ca > 90 && ca < 270)
+	{
 		*side = WE;
 		return (&map->west_img);
 	}
--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 00:40:14 by llevasse          #+#    #+#             */
-/*   Updated: 2023/12/08 19:33:07 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/12/16 19:20:55 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	img_pix_put(t_img *img, int x, int y, int colour)
 {
 	char	*pixel;
 	int		i;
-	
+
 	if (x < 0 || x > WINDOW_W || y < 0 || y > WINDOW_H)
 		return ;
 	i = img->bpp - 8;
