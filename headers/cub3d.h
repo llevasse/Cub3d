1c56bd6ae19556c7f78a967a4e7f321a6254374e
Leo <leonardlevasseur@yahoo.com>
2023-12-20 14:38:32 +0100

    pre implementation of doors

--- headers/cub3d.h
+++ headers/cub3d.h
@@ -6,25 +6,25 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 21:43:29 by llevasse          #+#    #+#             */
-/*   Updated: 2023/12/18 21:48:14 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/12/20 14:36:41 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define WINDOW_W	1920
-# define WINDOW_H	1200
+# define WINDOW_W	960 //1920
+# define WINDOW_H	600 //1200
 
 # define MMAP_W_RGB		0x000000
 # define MMAP_RGB		0xffffff
 # define MMAP_RANGE		8
-# define MMAP_BLOCK_S	16
+# define MMAP_BLOCK_S	8 //16
 
 # define PLAYER_RGB		0x0000ff
 # define PLAYER_FOV		80
 
-# define PLAYER_R_OFFSET	1
+# define PLAYER_R_OFFSET	3
 
 # define PI 3.1415926535
 # define RADIAN 0.017453292519444445
