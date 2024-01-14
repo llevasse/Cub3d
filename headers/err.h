b3693a0ab4e679e678f1c6a51bd038335fb845c9
llevasse <leonardlevasseur@yahoo.com>
2024-01-14 12:20:29 +0100

    idk

--- headers/err.h
+++ headers/err.h
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 23:42:30 by llevasse          #+#    #+#             */
-/*   Updated: 2024/01/09 15:30:32 by llevasse         ###   ########.fr       */
+/*   Updated: 2024/01/09 15:34:33 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
 # define S_WALL_OPEN_ERR "Error\nError in opening south wall texture file.\n"
 # define E_WALL_OPEN_ERR "Error\nError in opening east wall texture file.\n"
 # define W_WALL_OPEN_ERR "Error\nError in opening west wall texture file.\n"
+# define DOOR_OPEN_ERR "Error\nError in opening door texture file.\n"
 # define N_WALL_MISSING "Error\nNorth wall texture file is missing.\n"
 # define S_WALL_MISSING "Error\nSouth wall texture file is missing.\n"
 # define E_WALL_MISSING "Error\nEast wall texture file is missing.\n"
 # define W_WALL_MISSING "Error\nWest wall texture file is missing.\n"
-# define DOOR_OPEN_ERR "Error\nError in opening door texture file.\n"
 # define F_ERR "Error\nInvalid floor colour.\n"
 # define C_ERR "Error\nInvalid floor colour.\n"
 # define CUB_OPEN_ERR "Error\nError in opening cub file.\n"
